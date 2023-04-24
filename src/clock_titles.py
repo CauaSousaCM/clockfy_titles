399201b7f6d05437a5ec2cee7537321155d92e83
MarinhoDevQA <125994651+DevsQA@users.noreply.github.com>
2023-04-24 16:13:44 -0300

    Update repository

---
+++ src/clock_titles.py
@@ -0,0 +1,15 @@
+from utils import messages
+
+
+def return_title_for_clockfy():
+    try:
+        title = str(input(
+            messages.error_message(
+                "INSIRA O TITULO PARA SEU CLOCKFY: "))).upper()
+        print(messages.sucess_message(title))
+        return messages.sucess_message(title)
+    except Exception as error:
+        raise Exception(messages.error_message(
+            "HOUVE UM ERRO AO UTILIZAR MENSAGEM:\n"
+            +f"{error}"))
+return_title_for_clockfy()
